096c27a395bc1e5911b927166811fa9f45a6dd59
Paweł Goliński <pawel@pawelgolinski.pl>
2017-06-27 12:56:17 +0200

    indywidualne artykuły

--- src/InvitationBundle/Controller/DashboardController.php
+++ src/InvitationBundle/Controller/DashboardController.php
@@ -3,6 +3,7 @@
 namespace InvitationBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use InvitationBundle\Entity\News;
 
 class DashboardController extends Controller {
     public function indexAction() {
@@ -13,13 +14,17 @@ class DashboardController extends Controller {
         $Event = $Invitation->getEvent();
         
         $NewsCollection = $em->createQuery(
-                'SELECT n FROM InvitationBundle:News n WHERE 
-                n.event = :Event AND 
+                'SELECT n FROM InvitationBundle:News n
+                LEFT JOIN n.invitations ni
+                WHERE 
+                ((n.event = :Event AND n.range = '.News::RANGE_EVENT.') OR
+                (ni = :Invitation AND n.range = '.News::RANGE_INVITATION.')) AND
                 n.published = 1 AND
                 n.publishAt < CURRENT_TIMESTAMP()
                 ORDER BY n.createdAt DESC'
             )
             ->setParameter('Event', $Event)
+            ->setParameter('Invitation', $Invitation)
             ->getResult();
         return $this->render('InvitationBundle:Dashboard:index.html.twig', array(
             'Invitation' => $Invitation,
