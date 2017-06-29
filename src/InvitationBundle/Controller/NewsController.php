b88e44c9d411ea652b0f21c0ed4a2a67f01b59ee
pawel <pawel@dark-knight>
2017-06-29 21:03:16 +0200

    poprawki z Maćkiem

--- src/InvitationBundle/Controller/NewsController.php
+++ src/InvitationBundle/Controller/NewsController.php
@@ -3,6 +3,7 @@
 namespace InvitationBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use InvitationBundle\Entity\News;
 
 class NewsController extends Controller
 {
@@ -24,13 +25,17 @@ class NewsController extends Controller
         $em = $this->getDoctrine()->getManager();
         
         $NewsCollection = $em->createQuery(
-                'SELECT n FROM InvitationBundle:News n WHERE 
-                n.event = :Event AND 
+                'SELECT n FROM InvitationBundle:News n 
+                LEFT JOIN n.invitations ni
+                WHERE 
+                ((n.event = :Event AND n.range = '.News::RANGE_EVENT.') OR
+                (ni = :Invitation AND n.range = '.News::RANGE_INVITATION.')) AND
                 n.published = 1 AND
                 n.publishAt < CURRENT_TIMESTAMP()
                 ORDER BY n.createdAt DESC'
             )
             ->setParameter('Event', $Event)
+            ->setParameter('Invitation', $Invitation)
             ->getResult();
         
         $this->breadcrumb($News, $Event);
